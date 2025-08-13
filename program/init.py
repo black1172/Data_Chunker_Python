0da24a9c140ea004d2a057c6ebb9091b1b51fe02
black1172 <black.1172@buckeyemail.osu.edu>
2025-08-13 11:35:29 -0400

    order changes to init file

--- program/init.py
+++ program/init.py
@@ -4,13 +4,6 @@ import chunk_text as c
 import meta_data as m
 
 class PDFProcessor:
-    def __init__(self, filename):
-        self.name = filename
-        self.text = self.read_pdf()
-        self.tokens = self.tokenize()
-        self.chunks = self.chunk()
-        self.meta_data = self.add_metadata()
-
     def read_pdf(self):
         import pdf_reader as r
         return r.read_file(self.name)
@@ -26,4 +19,11 @@ class PDFProcessor:
     def add_metadata(self):
         import meta_data as m
         return m.fill_meta_data(self.chunks, filename=self.name)
+    
+    def __init__(self, filename):
+        self.name = filename
+        self.text = self.read_pdf()
+        self.tokens = self.tokenize()
+        self.chunks = self.chunk()
+        self.meta_data = self.add_metadata()
 
--- program/main.py
+++ program/main.py
@@ -1,4 +1,6 @@
 import init
 import json_methods as j
-test1 = init.PDFProcessor("(raw_pdf/test1.pdf")
+
+# main
+test1 = init.PDFProcessor("raw_pdf/test1.pdf")
 j.write_to_json(test1.meta_data)
