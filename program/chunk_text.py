37ccfe0743fa472bb4a69da817d5c12af88b6c6d
black1172 <black.1172@buckeyemail.osu.edu>
2025-08-13 10:52:00 -0400

    chunking method

--- program/chunk_text.py
+++ program/chunk_text.py
@@ -1,5 +1,14 @@
-import tiktoken as tt
+def chunk(tokens, chunk_size=400, overlap=80):
 
-def chunk(tokens):
-    
-    pass
+    # Declarations
+    chunks = []
+    start_index = 0
+
+    # Chunking Loop
+    while start_index < len(tokens):
+        end_index = start_index + chunk_size
+        chunk_tokens = tokens[start_index:end_index]
+        chunks.append(chunk_tokens)
+        start_index += chunk_size - overlap  # move forward, keep overlap
+
+    return chunks
