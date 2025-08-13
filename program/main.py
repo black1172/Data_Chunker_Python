86853e3994fa484f6843a78a60735ea53096f180
black1172 <black.1172@buckeyemail.osu.edu>
2025-08-13 15:28:39 -0400

    setup mass pdf processsing

--- program/main.py
+++ program/main.py
@@ -1,6 +1,13 @@
 import init
 import json_methods as j
+import os
 
 # main
-test1 = init.PDFProcessor("C:/VS_CODE/Data_Chunker_Python/raw_pdfs/burnout-flyer.pdf")
-j.write_to_json(test1.meta_data)
+all_data = []
+for current_pdf in os.listdir(r"C:/VS_CODE/Data_Chunker_Python/raw_pdfs"):
+    if current_pdf.lower().endswith(".pdf"):  # ensure it's a PDF
+        pdf_path = os.path.join(r"C:/VS_CODE/Data_Chunker_Python/raw_pdfs", current_pdf)
+        current_pdf = init.PDFProcessor(pdf_path)  # pass full path
+        all_data.extend(current_pdf.meta_data)     # add chunks to list
+
+j.write_to_json(all_data)
