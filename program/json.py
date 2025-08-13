13261c658509d935b7e4728e894f92c8984dfb45
black1172 <black.1172@buckeyemail.osu.edu>
2025-08-13 11:11:14 -0400

    typo

--- program/json.py
+++ program/json.py
@@ -2,5 +2,5 @@ import json
 
 def write_to_json(meta_chunks):
     with open("chunks.json", "w", encoding="utf-8") as chunked_file:
-        json.dump(meta_chunks, f, ensure_ascii=False, indent=2)
+        json.dump(meta_chunks, chunked_file, ensure_ascii=False, indent=2)
     return chunked_file
