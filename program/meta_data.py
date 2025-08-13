5f30a2e60ffdbadcd85ab470042d6568b3634333
black1172 <black.1172@buckeyemail.osu.edu>
2025-08-13 11:00:06 -0400

    meta data method filled

--- program/meta_data.py
+++ program/meta_data.py
@@ -1,1 +1,24 @@
 import datetime as dt
+
+def fill_meta_data(chunks, filename, encoding, tags):
+    
+    # Handle empty tags
+    if tags is None:
+            tags = []
+
+    # Keep track of datas date
+    date_str = dt.date.today().isoformat()
+    meta_chunks = []
+
+    # Loop to all chunks and add meta data
+    for i, chunk_tokens in enumerate(chunks):
+        meta_chunks.append({
+            "id": f"{filename}_{i}",
+            "title": filename.replace("_", " ").title(),
+            "source": filename,
+            "tags": tags,
+            "date": date_str,
+            "text": encoding.decode(chunk_tokens)
+        })
+
+    return meta_chunks
