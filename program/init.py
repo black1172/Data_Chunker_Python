617ca89c47d7095b1f888139477f1f800940a2ff
black1172 <black.1172@buckeyemail.osu.edu>
2025-08-13 11:14:35 -0400

    changes to init (needed to add class)

--- program/init.py
+++ program/init.py
@@ -3,9 +3,10 @@ import tokenize_text as t
 import chunk_text as c
 import meta_data as m
 
-def __init__(self, filename):
-    self.name = filename
-    self.text = r.read_file(self.name) # read to text
-    self.tokens = t.tokenize(self.text) # tokenize
-    self.chunks = c.chunk(self.tokens) # chunk
-    self.meta_data = m.fill_meta_data(self.chunks) # meta data
+class PDFProcessor:
+    def __init__(self, filename):
+        self.name = filename
+        self.text = r.read_file(self.name)              # PDF → text
+        self.tokens = t.tokenize(self.text)             # text → tokens
+        self.chunks = c.chunk(self.tokens)              # tokens → chunks
+        self.meta_data = m.fill_meta_data(self.chunks, filename=self.name)  # chunks → metadata
