4cedd4a6703b2bbcc2ae755e4cad601925bcd85c
black1172 <black.1172@buckeyemail.osu.edu>
2025-08-13 14:33:17 -0400

    changes to json writing

--- program/json_methods.py
+++ program/json_methods.py
@@ -1,6 +1,11 @@
 import json
+import os
 
 def write_to_json(meta_chunks):
-    with open("chunks.json", "w", encoding="utf-8") as chunked_file:
+     # Full file path
+    file_path = os.path.join("C:\VS_CODE\Data_Chunker_Python\output", "chunks.json")
+    
+    # Write JSON
+    with open(file_path, "w", encoding="utf-8") as chunked_file:
         json.dump(meta_chunks, chunked_file, ensure_ascii=False, indent=2)
     return chunked_file
