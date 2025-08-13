b5d49942b13d07f5ec0d75495e8f6d4d5ac99785
black1172 <black.1172@buckeyemail.osu.edu>
2025-08-13 11:01:07 -0400

    updated init to add meta data

--- program/init.py
+++ program/init.py
@@ -1,9 +1,11 @@
 import pdf_reader as r
 import tokenize_text as t
 import chunk_text as c
+import meta_data as m
 
 def __init__(self, filename):
     self.name = filename
     self.text = r.read_file(self.name) # read to text
     self.tokens = t.tokenize(self.text) # tokenize
     self.chunks = c.chunk(self.tokens) # chunk
+    self.meta_data = m.fill_meta_data(self.chunks) # meta data
