0ea1a44482b6077aab69b14de401223c89404e9f
black1172 <black.1172@buckeyemail.osu.edu>
2025-08-13 11:25:53 -0400

    readability and compartmentalization to init

--- program/init.py
+++ program/init.py
@@ -6,7 +6,24 @@ import meta_data as m
 class PDFProcessor:
     def __init__(self, filename):
         self.name = filename
-        self.text = r.read_file(self.name)              # PDF → text
-        self.tokens = t.tokenize(self.text)             # text → tokens
-        self.chunks = c.chunk(self.tokens)              # tokens → chunks
-        self.meta_data = m.fill_meta_data(self.chunks, filename=self.name)  # chunks → metadata
+        self.text = self.read_pdf()
+        self.tokens = self.tokenize()
+        self.chunks = self.chunk()
+        self.meta_data = self.add_metadata()
+
+    def read_pdf(self):
+        import pdf_reader as r
+        return r.read_file(self.name)
+
+    def tokenize(self):
+        import tokenize_text as t
+        return t.tokenize(self.text)
+
+    def chunk(self):
+        import chunk_text as c
+        return c.chunk(self.tokens)
+
+    def add_metadata(self):
+        import meta_data as m
+        return m.fill_meta_data(self.chunks, filename=self.name)
+
