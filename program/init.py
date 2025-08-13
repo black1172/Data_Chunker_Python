bf7306910cff686bd610f81ee0789ad28799652a
black1172 <black.1172@buckeyemail.osu.edu>
2025-08-13 10:55:01 -0400

    added small comments and meta_data python file

--- program/init.py
+++ program/init.py
@@ -4,6 +4,6 @@ import chunk_text as c
 
 def __init__(self, filename):
     self.name = filename
-    self.text = r.read_file(self.name)
-    self.tokens = t.tokenize(self.text)
-    self.chunks = c.chunk(self.tokens)
+    self.text = r.read_file(self.name) # read to text
+    self.tokens = t.tokenize(self.text) # tokenize
+    self.chunks = c.chunk(self.tokens) # chunk
