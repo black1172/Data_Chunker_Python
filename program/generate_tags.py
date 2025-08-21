b5f82d738d94f6bedb4e187321ad0d6469ad0677
Wyatt Black <black.1172@buckeyemail.osu.edu>
2025-08-20 21:53:20 -0400

    trying NLTK word rooting

--- program/generate_tags.py
+++ program/generate_tags.py
@@ -1,5 +1,9 @@
+
 import re
 from collections import Counter
+import nltk
+from nltk.stem import PorterStemmer
+from nltk import pos_tag, word_tokenize
 
 # Expanded stopwords list
 STOPWORDS = {
@@ -13,11 +17,30 @@ STOPWORDS = {
 }
 
 def generate_tags_from_text(text, top_n=8):
-    # Extract words and lowercase them
-    words = re.findall(r"\b[a-zA-Z]{3,}\b", text.lower())
-    # Remove stopwords
-    words = [w for w in words if w not in STOPWORDS]
-    # Count most common words
-    common_words = [word for word, _ in Counter(words).most_common(top_n)]
-    return common_words
+    # Download NLTK resources if not present
+    try:
+        nltk.data.find('tokenizers/punkt')
+    except LookupError:
+        nltk.download('punkt')
+    try:
+        nltk.data.find('taggers/averaged_perceptron_tagger')
+    except LookupError:
+        nltk.download('averaged_perceptron_tagger')
+
+    # Tokenize and lowercase
+    words = word_tokenize(text.lower())
+    # Remove stopwords and non-alpha
+    words = [w for w in words if w.isalpha() and w not in STOPWORDS]
+
+    # POS tagging and noun extraction
+    tagged = pos_tag(words)
+    nouns = [word for word, pos in tagged if pos.startswith('NN')]
+
+    # Stemming
+    stemmer = PorterStemmer()
+    stemmed_nouns = [stemmer.stem(word) for word in nouns]
+
+    # Count most common stemmed nouns
+    common_nouns = [word for word, _ in Counter(stemmed_nouns).most_common(top_n)]
+    return common_nouns
 
